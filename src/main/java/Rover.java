84c9812cbf88b1813566f5a66ab4f6528a3ca47e
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-03 22:59:13 +0200

    remove not needed test

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -2,10 +2,6 @@ public class Rover {
 
     private String position = "(0,0,N)";
 
-    public String position() {
-        return position;
-    }
-
     public String rotate() {
         if (position.equals("(0,0,E)"))
             return "(0,0,S)";
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -4,10 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoverTest {
 
-    @Test
-    void initial_position_is_zero() {
-        assertEquals("(0,0,N)", aRover().position());
-    }
 
     @Test
     void can_rotate() {
