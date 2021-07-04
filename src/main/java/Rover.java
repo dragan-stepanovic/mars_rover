a6ac7cf2e2a75a8815ca48806a6dde6584d4ddda
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-04 16:19:55 +0200

    still passing

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -3,12 +3,12 @@ public class Rover {
     private String position = "(0,0,N)";
 
     public String move() {
-        if (position.equals("(0,1,N)")) {
-            position = "(0,2,N)";
+        if (position.equals("(0,0,N)")) {
+            position = "(0,1,N)";
         } else if (position.equals("(0,2,N)")) {
             position = "(0,3,N)";
-        } else if (position.equals("(0,0,N)")) {
-            position = "(0,1,N)";
+        } else if (position.equals("(0,1,N)")) {
+            position = "(0,2,N)";
         }
 
         return position;
