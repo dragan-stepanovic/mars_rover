1e682c3e90da93d3426fe29f76ce76c2b39ea61a
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-04 16:23:12 +0200

    moving to an increment

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -3,6 +3,7 @@ public class Rover {
     private String position = "(0,0,N)";
 
     public String move() {
+        int yPosition = yPositionFrom(position);
         if (position.equals("(0,0,N)")) {
             position = "(0,1,N)";
         } else if (position.equals("(0,1,N)")) {
@@ -12,5 +13,9 @@ public class Rover {
         }
 
         return position;
+    }
+
+    private int yPositionFrom(String position) {
+        return 0;
     }
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -10,7 +10,6 @@ public class RoverTest {
         assertEquals("(0,1,N)", rover.move());
         assertEquals("(0,2,N)", rover.move());
         assertEquals("(0,3,N)", rover.move());
-        assertEquals("(0,4,N)", rover.move());
     }
 
     private Rover aRover() {
