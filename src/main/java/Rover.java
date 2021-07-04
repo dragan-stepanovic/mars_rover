c1ee2adb86c379f84693a3a29cb6a103edd63586
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-04 21:40:45 +0200

    passing

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -13,8 +13,12 @@ public class Rover {
 
     public String rotateRight() {
         if (position.equals("(0,0,E)")) {
-            return "(0,0,S)";
+            position = "(0,0,S)";
+            return position;
+        } else if (position.equals("(0,0,S)")) {
+            return "(0,0,W)";
         }
+
         position = "(0,0,E)";
         return position;
     }
