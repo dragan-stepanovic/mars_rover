35556be738ec8b4e430524061cccc136142e42f1
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-04 16:27:34 +0200

    rename test

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RoverTest {
 
     @Test
-    void can_move() {
+    void can_move_y() {
         Rover rover = aRover();
         assertEquals("(0,1,N)", rover.move());
         assertEquals("(0,2,N)", rover.move());
