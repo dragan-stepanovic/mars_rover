0c81d66372d1fde9a68c84c3da62a2ee587ead15
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-09 21:33:04 +0200

    reorder

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -13,6 +13,13 @@ public class RoverTest {
     }
 
     @Test
+    void can_move_by_x() {
+        final Rover rover = aRover();
+        rover.rotateRight();
+        assertEquals("(1,0,E)", rover.move());
+    }
+
+    @Test
     void can_rotate_right() {
         Rover rover = aRover();
         assertEquals("(0,0,E)", rover.rotateRight());
@@ -20,13 +27,6 @@ public class RoverTest {
         assertEquals("(0,0,W)", rover.rotateRight());
         assertEquals("(0,0,N)", rover.rotateRight());
         assertEquals("(0,0,E)", rover.rotateRight());
-    }
-
-    @Test
-    void can_move_by_x() {
-        final Rover rover = aRover();
-        rover.rotateRight();
-        assertEquals("(1,0,E)", rover.move());
     }
 
     private Rover aRover() {
