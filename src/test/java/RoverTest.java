4b6d91e78a48d7ab277e7017145fdd89e242a1c8
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-09 22:25:39 +0200

    rt

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -13,7 +13,7 @@ public class RoverTest {
     }
 
     @Test
-    void can_move_by_x_against_y_being_0() {
+    void can_move_by_x_with_y_being_0() {
         final Rover rover = aRover();
         rover.rotateRight();
         assertEquals("(1,0,E)", rover.move());
