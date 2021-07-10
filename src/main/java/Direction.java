ef32b8dbfe62214098b8d8e362bf57e2e35d6c81
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-10 16:01:19 +0200

    format

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -2,8 +2,8 @@ public class Direction {
 
     public static final char NORTH = 'N';
     public static final char EAST = 'E';
-    public static final char WEST = 'W';
     public static final char SOUTH = 'S';
+    public static final char WEST = 'W';
     private final char value;
 
     public Direction(char value) {
