2773baa7e45ae0b59ce94e1994c46b59f61b4a71
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-18 19:20:27 +0200

    inlining coordinates

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -3,6 +3,8 @@ public class Rover {
     public static final int INFINITE_ENOUGH_GRID_SIZE = 10000;
     private final Position position = Position.initial();
 
+    final Coordinates coordinates = Coordinates.initial();
+
     public String execute(String commands) {
         return this.execute(commands, INFINITE_ENOUGH_GRID_SIZE);
     }
