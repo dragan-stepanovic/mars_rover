430ea38748518225ea9f6fd2eabca746e121337f
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-20 13:34:46 +0200

    move constant

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,7 +1,7 @@
 public class Grid {
 
     public static final int MINIMUM_VALUE = 0;
-    public static final int INFINITE_ENOUGH_GRID_SIZE = 10000;
+    public static final Grid INFINITE_ENOUGH_GRID_SIZE = new Grid(10000);
     private final int size;
 
     public Grid(int size) {
