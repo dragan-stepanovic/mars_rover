ce905ded265bc0ea85d63eb9ca0c2025e8ecb71e
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-20 23:12:38 +0200

    reorder

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -13,6 +13,11 @@ public class RoverTest {
 
     private Rover rover;
 
+    @BeforeEach
+    public void initializeRover() {
+        rover = new Rover();
+    }
+
     static Stream<Arguments> wrappingTestInputs() {
         return Stream.of(
                 arguments("MM", 2, "0:0:N"),
@@ -25,23 +30,18 @@ public class RoverTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("wrappingTestInputs")
+    void wraps_around_the_grid(String commands, int gridSize, String result) {
+        assertEquals(result, rover.execute(commands, new Grid(gridSize)));
+    }
+
     static Stream<Arguments> interpretingTestInput() {
         return Stream.of(
                 arguments("L", "0:0:W"),
                 arguments("MMRMLMM", "1:4:N"),
                 arguments("RMMMLMMMMLLRM", "2:4:W"),
                 arguments("RMMLLRMMMRMMLM", "4:4:N"));
-    }
-
-    @BeforeEach
-    public void initializeRover() {
-        rover = new Rover();
-    }
-
-    @ParameterizedTest
-    @MethodSource("wrappingTestInputs")
-    void wraps_around_the_grid(String commands, int gridSize, String result) {
-        assertEquals(result, rover.execute(commands, new Grid(gridSize)));
     }
 
     @ParameterizedTest
