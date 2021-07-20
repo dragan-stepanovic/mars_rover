9593bb61d6a1cbc3dee96e154448c789f58933a2
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-20 13:34:13 +0200

    final

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -2,7 +2,7 @@ public class Grid {
 
     public static final int MINIMUM_VALUE = 0;
     public static final int INFINITE_ENOUGH_GRID_SIZE = 10000;
-    private int size;
+    private final int size;
 
     public Grid(int size) {
         this.size = size;
