a959519b2978ad10dca9e53ac6ac837716df88da
Dragan Stepanović <dragan.stepanovic@gmail.com>
2021-07-20 13:14:23 +0200

    made final

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -2,7 +2,7 @@ public class Rover {
 
     public static final int INFINITE_ENOUGH_GRID_SIZE = 10000;
 
-    private Coordinates coordinates = Coordinates.initial();
+    private final Coordinates coordinates = Coordinates.initial();
     private final Direction direction = Direction.initial();
 
     public String execute(String commands) {
